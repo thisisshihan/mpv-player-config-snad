cbe48fab4cdf04c68943672667bdcf46ef19a099
thisisshihan <38727829+thisisshihan@users.noreply.github.com>
2018-05-01 15:16:19 +0300

    Update visualizer.lua

--- scripts/visualizer.lua
+++ scripts/visualizer.lua
@@ -1,4 +1,7 @@
--- various audio visualization
+-- mpv various audio visualization
+-- This is a simplified visualizer by snad
+-- you can find original in "removed_scripts" folder
+-- this will work only in audio file with no album art 
 
 local opts = {
     mode = "noalbumart",
@@ -9,38 +12,12 @@ local opts = {
 
     name = "showcqtbar",
     -- off
-    -- showcqt
-    -- avectorscope
-    -- showspectrum
     -- showcqtbar
-    -- showwaves
 
     quality = "low",
-    -- verylow
-    -- low
-    -- medium
-    -- high
-    -- veryhigh
 
     height = 6,
     -- [4 .. 12]
-}
-
--- key bindings
--- cycle visualizer
-local cycle_key = "c"
-
-if not (mp.get_property("options/lavfi-complex", "") == "") then
-    return
-end
-
-local visualizer_name_list = {
-    "off",
-	"avectorscope",
-	"showspectrum",
-    "showcqt",
-    "showcqtbar",
-    "showwaves",
 }
 
 local axis_0 = "image/png;base64," ..
@@ -139,21 +116,9 @@ opts.height = math.floor(opts.height)
 local function get_visualizer(name, quality)
     local w, h, fps
 
-    if quality == "verylow" then
-        w = 640
-        fps = 30
-    elseif quality == "low" then
+    if quality == "low" then
         w = 960
         fps = 30
-    elseif quality == "medium" then
-        w = 1280
-        fps = 60
-    elseif quality == "high" then
-        w = 1920
-        fps = 60
-    elseif quality == "veryhigh" then
-        w = 2560
-        fps = 60
     else
         msg.log("error", "invalid quality")
         return ""
@@ -161,55 +126,7 @@ local function get_visualizer(name, quality)
 
     h = w * opts.height / 16
 
-    if name == "showcqt" then
-        local count = math.ceil(w * 180 / 1920 / fps)
-
-        return "[aid1] asplit [ao]," ..
-            "afifo, aformat     = channel_layouts = stereo," ..
-            "firequalizer       =" ..
-                "gain           = '1.4884e8 * f*f*f / (f*f + 424.36) / (f*f + 1.4884e8) / sqrt(f*f + 25122.25)':" ..
-                "scale          = linlin:" ..
-                "wfunc          = tukey:" ..
-                "zero_phase     = on:" ..
-                "fft2           = on," ..
-            "showcqt            =" ..
-                "fps            =" .. fps .. ":" ..
-                "size           =" .. w .. "x" .. h .. ":" ..
-                "count          =" .. count .. ":" ..
-                "csp            = bt709:" ..
-                "bar_g          = 2:" ..
-                "sono_g         = 4:" ..
-                "bar_v          = 9:" ..
-                "sono_v         = 17:" ..
-                "axisfile       = data\\\\:'" .. axis_0 .. "':" ..
-                "font           = 'Nimbus Mono L,Courier New,mono|bold':" ..
-                "fontcolor      = 'st(0, (midi(f)-53.5)/12); st(1, 0.5 - 0.5 * cos(PI*ld(0))); r(1-ld(1)) + b(ld(1))':" ..
-                "tc             = 0.33:" ..
-                "attack         = 0.033:" ..
-                "tlength        = 'st(0,0.17); 384*tc / (384 / ld(0) + tc*f /(1-ld(0))) + 384*tc / (tc*f / ld(0) + 384 /(1-ld(0)))'," ..
-            "format             = yuv420p [vo]"
-
-
-    elseif name == "avectorscope" then
-        return "[aid1] asplit [ao]," ..
-            "afifo," ..
-            "aformat            =" ..
-                "sample_rates   = 192000," ..
-            "avectorscope       =" ..
-                "size           =" .. w .. "x" .. h .. ":" ..
-                "r              =" .. fps .. "," ..
-            "format             = rgb0 [vo]"
-
-
-    elseif name == "showspectrum" then
-        return "[aid1] asplit [ao]," ..
-            "afifo," ..
-            "showspectrum       =" ..
-                "size           =" .. w .. "x" .. h .. ":" ..
-                "win_func       = blackman [vo]"
-
-
-    elseif name == "showcqtbar" then
+    if name == "showcqtbar" then
         local axis_h = math.ceil(w * 12 / 1920) * 4
 
         return "[aid1] asplit [ao]," ..
@@ -245,15 +162,6 @@ local function get_visualizer(name, quality)
             "vflip [v1];" ..
             "[v0][v1] vstack [vo]"
 
-
-    elseif name == "showwaves" then
-        return "[aid1] asplit [ao]," ..
-            "afifo," ..
-            "showwaves          =" ..
-                "size           =" .. w .. "x" .. h .. ":" ..
-                "r              =" .. fps .. ":" ..
-                "mode           = p2p," ..
-            "format             = rgb0 [vo]"
     elseif name == "off" then
         return "[aid1] afifo [ao]; [vid1] fifo [vo]"
     end
@@ -309,20 +217,3 @@ local function visualizer_hook()
 end
 
 mp.add_hook("on_preloaded", 50, visualizer_hook)
-
-local function cycle_visualizer()
-    local i, index = 1
-    for i = 1, #visualizer_name_list do
-        if (visualizer_name_list[i] == opts.name) then
-            index = i + 1
-            if index > #visualizer_name_list then
-                index = 1
-            end
-            break
-        end
-    end
-    opts.name = visualizer_name_list[index]
-    visualizer_hook()
-end
-
-mp.add_key_binding(b, "cycle-visualizer", cycle_visualizer)
