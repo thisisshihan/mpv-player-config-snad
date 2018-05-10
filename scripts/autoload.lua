e0210d01013d530b291b79cdc0929a3c8d58ceea
Shihan <this.is.shihan@gmail.com>
2018-05-10 11:51:34 +0300

    Added support for *.mov file.
    Added support for *.mov file.

--- scripts/autoload.lua
+++ scripts/autoload.lua
@@ -24,7 +24,7 @@ end
 
 EXTENSIONS = Set {
     'mkv', 'avi', 'mp4', 'ogv', 'webm', 'rmvb', 'flv', 'wmv', 'mpeg', 'mpg', 'm4v', '3gp',
-    'mp3', 'wav', 'ogm', 'flac', 'm4a', 'wma', 'ogg', 'opus',
+    'mp3', 'wav', 'ogm', 'flac', 'm4a', 'wma', 'ogg', 'opus', 'mov',
 }
 
 function add_files_at(index, files)
